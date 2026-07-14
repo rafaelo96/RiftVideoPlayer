ee991d31ddffdf5da1fae11e386e58514bda2e7d
rafaelo96 <rafaelo96@users.noreply.github.com>
2026-07-14 07:19:46 +0000

    deploy: 1eb21eea2477c3499769757685cca0da4e65c709

---
+++ _next/static/UzZsdMRIPlUJ8AgamxtOc/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/RiftVideoPlayer//_next/:path+",
+        "destination": "/RiftVideoPlayer/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
